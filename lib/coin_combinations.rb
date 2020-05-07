95546a9949877c12af48263f2ddfddc1c8b1c4a1
Ryan Duff <ryanduff613@gmail.com>
2020-05-07 10:45:03 -0700

    write spec to test that code creates an instance of CoinCombos and feeds the user_entry into an instance variable, code passes test

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,1 +1,18 @@
 require ('pry')
+
+class CoinCombos
+  
+
+  def initialize(user_entry)
+    @user_entry = user_entry
+    @coins = {"quarters" => 25, "dimes" => 10, "nickles" => 5, "pennies" => 1}
+  end
+
+  def user_entry
+    @user_entry
+  end
+
+  def counter
+    # code here
+  end  
+end  
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,4 +2,10 @@ require ('rspec')
 require ('pry')
 require ('coin_combinations')
 
-describe()
+describe("CoinCombos")do
+  it("it creates a new instance of CoinCombos and gives a value to the user_entry variable")do
+    coin_combo = CoinCombos.new(87)
+  expect(coin_combo.user_entry).to(eq(87))
+
+  end
+end
