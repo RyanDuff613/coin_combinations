392673812fbc28d9184295f7f0da39d0f10a0aa4
Ryan Duff <ryanduff613@gmail.com>
2020-05-07 16:23:46 -0700

    struggle to make loops work

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,18 +1,35 @@
 require ('pry')
 
 class CoinCombos
-  attr_reader(:user_entry)
+  attr_reader(:user_entry, :coin_values, :coin_string, :results)
+  attr_writer(:results, :user_entry)
+
 
   def initialize(user_entry)
     @user_entry = user_entry
-    @coins = {"quarters" => 25, "dimes" => 10, "nickles" => 5, "pennies" => 1}
+    # @coin_string = ["quarter","dime","nickle","pennie"]
+    @coin_values = [25,10,5,1]
+    @results = [0,0,0,0]
   end
-
-  # def user_entry
-  #   @user_entry
-  # end
-
-  def counter
-    # code here
+  
+  
+  # binding.pry
+  def coin_counter
+    
+    i = 0
+    loop do
+      if i > 3
+        break
+      end
+      
+      while (@coin_values[i] <= @user_entry) do
+        @user_entry -= @coin_values[i]
+        @results[i] += 1
+        i += 1
+      end
+     
+    end
   end  
+  
 end  
+
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -5,12 +5,12 @@ require ('coin_combinations')
 describe("CoinCombos")do
   it("it creates a new instance of CoinCombos and gives a value to the user_entry variable")do
     coin_combo = CoinCombos.new(87)
-  expect(coin_combo.user_entry).to(eq(87))
+    expect(coin_combo.user_entry).to(eq(87))
   end
   
   it("loops through coin_string & coin_numbers arrays to compare user input")do
     coin_combo = CoinCombos.new(87)
-    expect(CoinCombos.counter()).to(eq("3 quarters, 1 dime, 2 pennies"))
+    expect(coin_combo.results[0]).to(eq(3))
   end
 
 end
